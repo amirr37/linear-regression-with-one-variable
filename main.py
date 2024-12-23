a5665f2f18e02a814c9b8fef54d1ad50c951b51e
amir37 <amirrj037@gmail.com>
2024-12-23 19:11:45 +0330

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+"""
+
+linear regression with one variable project
+
+"""
+
+# Define the dataset
+data = {
+    "Size (sq ft)": [850, 900, 950, 1000, 1050, 1100, 1150, 1200, 1250, 1300],
+    "Price ($)": [180000, 200000, 220000, 240000, 260000, 280000, 300000, 320000, 340000, 360000]
+}
+
+# todo : create
+
+
